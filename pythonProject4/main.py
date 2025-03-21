1c8b6288fd9702f34bcde6c5a4cc66a7b645ee34
rozovyu <kuklapa3434@gmail.com>
2025-03-21 15:02:20 +0500

    Add files via upload

---
+++ pythonProject4/main.py
@@ -0,0 +1,5 @@
+from uravnenie import medium_arifmetic
+chislo3 = int(input())
+chislo4 =int(input())
+chislo5= int(input())
+print('Среднее арифметическое чисел',chislo3,',',chislo4,'и',chislo5,'-',medium_arifmetic(chislo3, chislo4, chislo5))
---
+++ pythonProject4/uravnenie.py
@@ -0,0 +1,2 @@
+def medium_arifmetic(chislo,chislo1,chislo2):
+    return (chislo + chislo1 + chislo2) // 3
